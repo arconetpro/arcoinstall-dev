7970ca17d89bc7bc6312735a7ebda79c421bd5cb
Erik Dubois <erik.dubois@gmail.com>
2024-12-06 18:36:40 +0100

    oups

--- archinstall/default_profiles/desktops/cosmic.py
+++ archinstall/default_profiles/desktops/cosmic.py
@@ -1,8 +1,14 @@
-from typing import override
+import shutil
+
+import archinstall
+from typing import TYPE_CHECKING, override
 
 from archinstall.default_profiles.profile import GreeterType, ProfileType
 from archinstall.default_profiles.xorg import XorgProfile
+from archinstall.lib.models import User
 
+if TYPE_CHECKING:
+	from archinstall.lib.installer import Installer
 
 class CosmicProfile(XorgProfile):
 	def __init__(self) -> None:
@@ -14,6 +20,23 @@ class CosmicProfile(XorgProfile):
 		return [
 			"cosmic",
 		]
+
+	@override
+	def post_install(self, install_session: 'Installer') -> None:
+		users: User | list[User] = archinstall.arguments.get('!users', [])
+		if not isinstance(users, list):
+			users = [users]
+
+		for user in users:
+			source = install_session.target / "etc" / "skel"
+			destination = install_session.target / "home" / user.username
+
+			try:
+				shutil.copytree(source, destination, dirs_exist_ok=True)
+				install_session.arch_chroot(f'chown -R {user.username}:{user.username} /home/{user.username}')
+				print(f"Copied {source} to {destination}")
+			except Exception as e:
+				print(f"Error copying configuration: {e}")
 
 	@property
 	@override
