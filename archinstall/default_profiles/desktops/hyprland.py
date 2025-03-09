effc37dc2f6ac229ce5ebb8463eb43bb7cec6f6f
Erik Dubois <erik.dubois@gmail.com>
2025-03-09 07:40:09 +0100

    2025 03 09

--- archinstall/default_profiles/desktops/hyprland.py
+++ archinstall/default_profiles/desktops/hyprland.py
@@ -1,6 +1,6 @@
 import shutil
-
 import archinstall
+
 from typing import TYPE_CHECKING, override
 
 from archinstall.default_profiles.desktops import SeatAccess
@@ -24,7 +24,7 @@ if TYPE_CHECKING:
 
 class HyprlandProfile(XorgProfile):
 	def __init__(self) -> None:
-		super().__init__('Hyprland', ProfileType.DesktopEnv, description='')
+		super().__init__('Hyprland', ProfileType.WindowMgr, description='')
 
 		self.custom_settings = {'seat_access': None}
 
